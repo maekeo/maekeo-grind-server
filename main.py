b90588da3e92d834ab22855d105d3e7e2f0d6681
maekeo <maeilcoffees@gmail.com>
2026-05-02 13:13:42 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -12,8 +12,10 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
-    allow_methods=["*"],
+    allow_credentials=False,
+    allow_methods=["GET", "POST", "OPTIONS"],
     allow_headers=["*"],
+    expose_headers=["*"],
 )
 
 class ImageRequest(BaseModel):
